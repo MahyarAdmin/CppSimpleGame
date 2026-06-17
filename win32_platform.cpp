c1e3dd38f4e87c0384aafdba50ffb7357027a66e
Mahyar <Mahyar.Abdullahi.1384@gmail.com>
2026-06-18 02:57:03 +0330

    change the name of the program for more clarity and possible new versions on other platforms, creating the first working window

---
+++ win32_platform.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+
+bool running = true;
+
+LRESULT CALLBACK windows_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
+
+    WNDCLASS windows_class = {};
+    windows_class.style = CS_HREDRAW | CS_VREDRAW;
+    windows_class.lpszClassName = TEXT("SimpleCppGame");
+    windows_class.lpfnWndProc = windows_callback;
+
+    RegisterClass(&windows_class);
+
+    CreateWindow(windows_class.lpszClassName, TEXT("My first ever game"), WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
+    while(running) {
+        MSG msg = {};
+        while(PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    return 0;
+}
+
+LRESULT CALLBACK windows_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    LRESULT result = {};
+    switch (uMsg){
+        case WM_CLOSE:
+        case WM_DESTROY: {
+            running = false;
+            return 0;
+        }
+        default: {
+            result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
+    }
+    return result;
+}
