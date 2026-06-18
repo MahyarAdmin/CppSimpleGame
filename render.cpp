7b0915ae3a30edb13d5a437b94704386ca1a11f4
Mahyar <Mahyar.Abdullahi.1384@gmail.com>
2026-06-18 15:53:19 +0330

    Creating a new draw function for rectangles to create a fixed size shape for every screen size

--- render.cpp
+++ render.cpp
@@ -1,6 +1,8 @@
 #include "C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um\wingdi.h"
 #include "utility.cpp"
 
+const float RENDER_SCALE = 0.01f;
+
 struct render{
     int width, height;
     void* memory = nullptr;
@@ -28,7 +30,7 @@ void clear_screen(unsigned int color) {
     }
 }
 
-void draw_rect(int x0, int y0, int x1, int y1, unsigned int color) {
+void draw_rectPix(int x0, int y0, int x1, int y1, unsigned int color) {
 
     x0 = clampData(0, x0, renderData.width);
     x1 = clampData(0, x1, renderData.width);
@@ -41,4 +43,22 @@ void draw_rect(int x0, int y0, int x1, int y1, unsigned int color) {
             *pixel++ = color;
         }
     }
+}
+
+void draw_rect(float x, float y, float halfSizeX, float halfSizeY, unsigned int color) {
+
+    x *= renderData.height * RENDER_SCALE;
+    y *= renderData.height * RENDER_SCALE;
+    halfSizeX *= renderData.height * RENDER_SCALE;
+    halfSizeY *= renderData.height * RENDER_SCALE;
+
+    x += renderData.width / 2.f;
+    y += renderData.height / 2.f;
+    
+    int x0 = x - halfSizeX;
+    int x1 = x + halfSizeX;
+    int y0 = y - halfSizeY;
+    int y1 = y + halfSizeY;
+
+    draw_rectPix(x0, y0, x1, y1, color);
 }
